0a52a6a74bc5700a40ca973566d90ce1e6c2c1e3
Ivan-Cecchi <i.cecchi@outlook.com>
2024-07-06 17:40:23 +0200

    Initial commit

---
+++ src/main/java/it/epicode/RallyRent/RallyRentApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.RallyRent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RallyRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RallyRentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/RallyRent/RallyRentApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.RallyRent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RallyRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
